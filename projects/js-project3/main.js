de257f91298fa12984f2d8bd41c31e1e77ca1381
Abhijeet Thombare <abhithombare45@gmail.com>
2025-08-02 09:23:28 +0530

    Working on Basic coading in js and radio buttion on project with click action on it.

--- projects/js-project3/main.js
+++ projects/js-project3/main.js
@@ -10,12 +10,33 @@ document.querySelector("#close-nav-menu").addEventListener("click", function(){
 
 // Greeting section 
 
+function celsiusToFahr(temperature){
+     let fahr = (temperature * 9/5)+32;
+     return fahr;
+}
+
+celsiusToFahr(25);
+
 const greetingText = "Good Evening!";
 const weatherCondition = "cludy";
 const userLocation = "Pune";
-let temperature = 26.8673;
-let weatherText = `The weather is ${weatherCondition} in ${userLocation} and it's ${temperature.toFixed(1)}°C outside.`
+let temperature = 25;
+
+let celsiusText = `The weather is ${weatherCondition} in ${userLocation} and it's ${temperature.toFixed(1)}°C outside.`
+let fahrText = `The weather is ${weatherCondition} in ${userLocation} and it's ${celsiusToFahr(temperature).toFixed(1)}°F outside.`
 
 
 document.querySelector("#greeting").innerHTML = greetingText;
-document.querySelector("p#weather").innerHTML = weatherText; 
+
+document.querySelector(".weather-group").addEventListener("click",function(e){
+     // console.log(e.target.id)
+     // fahr 
+     // celsius
+
+     if (e.target.id == "celsius") {
+          document.querySelector("p#weather").innerHTML = celsiusText; 
+     } else if (e.target.id == "fahr") {
+          document.querySelector("p#weather").innerHTML = fahrText; 
+     }
+
+}); 
