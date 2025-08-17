8f77149ff8f957a5e0d2cd57392e6575788c0270
Abhijeet Thombare <abhithombare45@gmail.com>
2025-08-18 03:15:00 +0530

    Auto COMMIT on React Tut Project

--- react_tutorials/PROJECTS/jersey-shop-initial/src/App.jsx
+++ react_tutorials/PROJECTS/jersey-shop-initial/src/App.jsx
@@ -94,21 +94,19 @@ function App() {
                 <h4>{shopName}</h4>
 
                 { items.map(item => 
-                    <div key={item.id} className="product">
+                    <div key={item.id} className={`product ${item.isInBag ? 'selected' : ''}`}>
                         <div className="photo">
                             <img src={"./img/" + item.photo} />
                         </div>
                         <div className="description">
                             <span className="name">{item.name}</span>
                             <span className="price">{item.price}</span>
-                            { item.isInBag ?
+                            { item.isInBag &&
                                 <div className="quantity-area">
                                     <button>-</button>
                                     <span className="quantity">{item.quantity}</span>
                                     <button>+</button>
                                 </div>
-
-                                : ""
                             }
                         </div>
                     </div>
