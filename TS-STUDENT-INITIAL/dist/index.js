905e4900c65a65da0d53c0c82358a187bc61cb6d
Abhijeet Thombare <abhithombare45@gmail.com>
2025-08-18 03:52:36 +0530

    global commit.

--- TS-STUDENT-INITIAL/dist/index.js
+++ TS-STUDENT-INITIAL/dist/index.js
@@ -6,41 +6,15 @@ function addRow(table, student) {
     const name = tr.insertCell();
     name.appendChild(document.createTextNode(`${student.firstName} ${student.lastName}`));
     const age = tr.insertCell();
-    age.appendChild(document.createTextNode((new Date().getFullYear() - parseInt(student.birthYear)).toString()));
+    age.appendChild(document.createTextNode(33., toString()));
     const majors = tr.insertCell();
-    if (student.focusArea) {
-        if (typeof student.focusArea == "string") {
-            majors.appendChild(document.createTextNode(student.focusArea));
-        }
-        else {
-            let areas = "";
-            student.focusArea.forEach(area => {
-                areas += area + ", ";
-            });
-            majors.appendChild(document.createTextNode(areas.slice(0, -2)));
-        }
-    }
-    else {
-        majors.appendChild(document.createTextNode("--"));
-    }
+    majors.appendChild(document.createTextNode("Space Engineering, Civil Engineering"));
     const status = tr.insertCell();
-    if (student.dateRegistrationSuspended) {
-        status.appendChild(document.createTextNode("Inactive"));
-    }
-    else {
-        status.appendChild(document.createTextNode("Active"));
-    }
+    status.appendChild(document.createTextNode("Active"));
 }
 // select HTML table
 function selectTable() {
     return document.querySelector("#students-table");
 }
-function refreshTable(table, students) {
-    table.querySelector("tbody").innerHTML = "";
-    students.forEach(student => {
-        addRow(table, student); // add a row        
-    });
-}
-window.onload = function () {
-    refreshTable(selectTable(), students);
-};
+// add a row
+// addRow(selectTable());
