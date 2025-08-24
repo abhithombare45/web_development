864eb2a836228a6ed34c2d6dbcbec7518bb22148
Abhijeet Thombare <abhithombare45@gmail.com>
2025-08-25 01:12:00 +0530

    Auto COMMIT on React Tut Project

--- Portfolio/abhithombare45/script.js
+++ Portfolio/abhithombare45/script.js
@@ -1,2 +1,14 @@
 // Will add interactivity (navbar toggle, animations) later
 console.log("Portfolio loaded successfully!");
+
+
+window.addEventListener("scroll", function () {
+    const header = document.querySelector("header");
+    if (window.scrollY > 50) {
+        header.style.background = "rgba(0, 0, 0, 0.5)";
+        header.style.backdropFilter = "blur(6px)";
+    } else {
+        header.style.background = "transparent";
+        header.style.backdropFilter = "none";
+    }
+});
