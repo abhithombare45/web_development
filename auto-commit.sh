1f2af98210cb56f73bbefdc78c91bf630773c932
Abhijeet Thombare <abhithombare45@gmail.com>
2025-08-25 14:21:00 +0530

    Auto COMMIT on Portfolio Project

---
+++ auto-commit.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cd /Users/abhijeetthombare/ab_lib/web-dev/web_development
+date >> heartbeat.log
+/usr/bin/git add .
+/usr/bin/git commit -m "Auto COMMIT on Portfolio Project" # For Porfolio
+#/usr/bin/git commit -m "Auto COMMIT on HTML, CSS & JS - Project" # For Project & Understandings.
+/usr/bin/git push origin main
